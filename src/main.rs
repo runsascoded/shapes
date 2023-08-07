c3597006d31a9e9bb1f2324366b899925c2154f3
Ryan Williams <ryan@runsascoded.com>
2023-08-07 17:16:51 -0400

    snapshot

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,67 @@
-use std::f64::consts::PI;
+use std::{f64::consts::PI, ops::{Add, Mul, Deref, Div}};
 
 use autodiff::*;
+
+//type F3 = (f64, f64, f64);
+#[derive(Debug)]
+pub struct F3([f64; 3]);
+
+#[derive(Debug)]
+pub struct D(F<f64, F3>);
+
+impl Deref for F3 {
+    type Target = [f64; 3];
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Deref for D {
+    type Target = F<f64, F3>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+// pub trait Z(Zero);
+
+impl Add for F3 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        F3([self[0] + rhs[0], self[1] + rhs[1], self[2] + rhs[2]])
+    }
+}
+
+impl Zero for F3 {
+    fn zero() -> Self {
+        F3([0.0, 0.0, 0.0])
+    }
+
+    fn is_zero(&self) -> bool {
+        self[0] == 0.0 && self[1] == 0.0 && self[2] == 0.0
+    }
+}
+
+// impl Mul for F3 {
+//     type Output = Self;
+//     fn mul(self, rhs: Self) -> Self {
+//         F3([self[0] * rhs[0], self[1] * rhs[1], self[2] * rhs[2]])
+//     }
+// }
+
+impl Mul<f64> for F3 {
+    type Output = Self;
+    fn mul(self, rhs: f64) -> Self {
+        F3([self[0] * rhs, self[1] * rhs, self[2] * rhs])
+    }
+}
+
+impl Div<f64> for F3 {
+    type Output = Self;
+    fn div(self, rhs: f64) -> Self {
+        F3([self[0] / rhs, self[1] / rhs, self[2] / rhs])
+    }
+}
 
 // trait Area {
 //     fn area<T: Float>(&self) -> T;
@@ -22,26 +83,51 @@ impl FromFloat for FT<f64> {
     }
 }
 
-pub struct Point<T: Float> {
+impl FromFloat for D {
+    fn from<T>(f: f64) -> Self {
+        D(F::cst(f))
+    }
+}
+
+pub struct Point<T> {
     x: T,
     y: T,
 }
 
-pub struct Circle<T: Float> {
+pub struct Circle<T> {
     c: Point<T>,
     r: T,
 }
 
-impl<T: Float + FromFloat> Circle<T> {
+impl<T: Mul<T, Output = T> + Mul<f64, Output = T> + FromFloat> Circle<T>
+{
     pub fn area(&self) -> T {
-        let pi: T = FromFloat::from::<T>(PI);
-        pi * self.r * self.r
+        // let pi: T = FromFloat::from::<T>(PI);
+        let pi_r: T = self.r * PI;
+        pi_r * self.r
     }
 }
 
+// use autodiff::F::var;
+
 fn main() {
-    let c1 = Circle { r: 1.0, c: Point { x: 2.0, y: 3.0 } };
-    let c2: Circle<FT<f64>> = Circle { r: F::var(2.0), c: Point { x:F::var(-1.0), y: F::var(-1.0) } };
+    // let var = F::var;
+    let z = F3([0.0, 0.0, 0.0]);
+    let r: D = D(F::new(1.0, z));
+    let x: D = D(F::new(0.0, z));
+    let y: D = D(F::new(0.0, z));
+    let c: Point<D> = Point { x, y };
+    let c1 = Circle {
+        r,
+        c,
+     };
+    // let c2: Circle<FT<f64>> = Circle {
+    //     r: F::new(1.0, F3([1, 0, 0])),
+    //     c: Point {
+    //         x: F::new(1.0, F3([0, 1, 0])),
+    //         y: F::new(1.0, F3([0, 0, 1])),
+    //      },
+    //  };
     // let c3 = Circle { r: 2 };
 
     dbg!("c1: {}", c1.area());
