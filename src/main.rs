cdd60b4a7b0d41ff11286251a57c40721456f67b
Ryan Williams <ryan@runsascoded.com>
2023-08-08 11:16:06 -0400

    quadratic checkpoint

--- src/main.rs
+++ src/main.rs
@@ -31,20 +31,52 @@ fn main() {
         let cx: Tensor<'_, f64> = ctx.placeholder("cx", &[]);
         let cy: Tensor<'_, f64> = ctx.placeholder("cy", &[]);
         let r: Tensor<'_, f64> = ctx.placeholder("r", &[]);
-        let C: Tensor<'_, f64> = cx*cx + cy+cy;
-        // let D = C - r*r + 1;
-        // let b2a = cx*D / 2 / C;
+        let C: Tensor<'_, f64> = cx*cx + cy*cy;
+        let D = C - r*r + 1.;
+        let a = 4*C;
+        let b = -4*cx*D;
+        let c = D*D - 4*cy*cy;
 
+        let d = b*b - 4.*a*c;
+        let dsqrt = d.sqrt();
+        let rhs = dsqrt / 2. / a;
+        let lhs = -b / 2. / a;
+        let x0 = lhs - rhs;
+        let x1 = lhs + rhs;
         // fn eval(y: &Tensor<'_, f64>, x: impl Placeholder, v: f64, ctx: &mut ag::Context<f64>) -> Result<f64, ag::EvalError> {
         //     let feed = ag::ndarray::arr0(v);
         //     return ctx.evaluator().push(y).feed(x, feed.view()).run()[0];
         // }
 
+        let d0 = &grad(&[x0], &[cx, cy, r]);
+        //let d1 = &grad(&[x1], &[cx, cy, r]);
         // let z = 2.*x*x + 3.*y + 1.;
-        let dcdx = &grad(&[C], &[cx])[0];
-        let feed = ag::ndarray::arr0(2.);
-        let ran = ctx.evaluator().push(dcdx).feed(cx, feed.view()).run();
-        let val = &ran[0];
+        // let dc = &grad(&[C], &[cx, cy]);
+        let dd = &grad(&[D], &[cx, cy]);
+        let ran =
+            ctx
+            .evaluator()
+            .push(dd[0])
+            .feed(cx, ag::ndarray::arr0(2.).view())
+            .push(dd[1])
+            .feed(cy, ag::ndarray::arr0(3.).view())
+            .run();
+        // println!("rx {:?}", &ran[0].as_ref().unwrap());
+        // println!("ry {:?}", &ran[1].as_ref().unwrap());
+        println!("ran {:?}", ran);
+
+        // let val = &ran[0].as_ref().unwrap();
+        // println!("dcdx: {} ({:?})", val, val);
+
+        // match val {
+        //     Ok(val) => {
+        //         println!("dcdx: {:?}", val);
+        //         println!("val: {}", val);
+        //     },
+        //     Err(err) =>
+        //     println!("dcdx: err {:?}", err),
+        // }
+
         // let val = {
         //     let feed = ag::ndarray::arr0(2.);
         //     let pushed = ctx.evaluator().push(dcdx);
@@ -52,14 +84,7 @@ fn main() {
         //     let ran = fed.run();
         //     &ran[0]
         // };
-        match val {
-            Ok(val) => {
-                println!("dcdx: {:?}", val);
-                println!("val: {}", val);
-            },
-            Err(err) =>
-            println!("dcdx: err {:?}", err),
-        }
+
         // let if Ok(val) = val {
         //     println!("dCdx: {:?}", val);
         // } else {
