c4d33a77ccc997da29da2c3031295923134daf09
Ryan Williams <ryan@runsascoded.com>
2023-08-09 11:56:19 -0400

    num-dual

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,52 @@
-use ag::ndarray::{arr0, self, IxDyn};
-use autograd as ag;
-use ag::tensor_ops as T;
-use ag::{tensor_ops::*, Tensor, VariableEnvironment, variable::GetVariableTensor};
+use num_dual::*;
+use nalgebra::SMatrix;
+
+fn f<D: DualNum<f64> + PartialOrd + Copy>(cx: D, cy: D, r: D) -> SMatrix<D, 4, 1> {
+    let cx2 = cx * cx;
+    let cy2 = cy * cy;
+    let C = cx2 + cy2;
+    let r2 = r * r;
+    let D = C - r2 + 1.;
+    let a = C * 4.;
+    let b = cx * D * -4.;
+    let c = D*D - cy2 * 4.;
+
+    let d = b*b - a*c*4.;
+    let dsqrt = d.sqrt();
+    let rhs = dsqrt / 2. / a;
+    let lhs = -b / 2. / a;
+
+    let x0 = lhs - rhs;
+    let y0sq = -x0*x0 + 1.;
+    let y0_0 = y0sq.sqrt();
+    let y0_1 = -y0_0;
+    let x0cx = x0 - cx;
+    let x0cx2 = x0cx * x0cx;
+    let y0_0cy = y0_0 - cy;
+    let y0_1cy = y0_1 - cy;
+    let check0_0 = (x0cx2 + y0_0cy*y0_0cy - r2).abs();
+    let check0_1 = (x0cx2 + y0_1cy*y0_1cy - r2).abs();
+    let y0 = if check0_0 < check0_1 { y0_0 } else { y0_1 };
+    println!("checks0: {:?}, {:?}", check0_0, check0_1);
+
+    let x1 = lhs + rhs;
+    let y1sq = -x1*x1 + 1.;
+    let y1_0 = y1sq.sqrt();
+    let y1_1 = -y1_0;
+    let x1cx = x1 - cx;
+    let x1cx2 = x1cx * x1cx;
+    let y1_0cy = y1_0 - cy;
+    let y1_1cy = y1_1 - cy;
+    let check1_0 = (x1cx2 + y1_0cy*y1_0cy - r2).abs();
+    let check1_1 = (x1cx2 + y1_1cy*y1_1cy - r2).abs();
+    let y1 = if check1_0 < check1_1 { y1_0 } else { y1_1 };
+    println!("checks1: {:?}, {:?}", check1_0, check1_1);
+
+    return SMatrix::from([ x0, y0, x1, y1 ]);
+}
 
 fn main() {
-    let mut env = VariableEnvironment::new();
-    let cx = env.slot().set(arr0(1.));
-    let cy = env.slot().set(arr0(0.1));
-    let r = env.slot().set(arr0(1.));
-    env.run(|g| {
-        let cx = g.variable(cx);
-        let cy = g.variable(cy);
-        let r = g.variable(r);
-
-        let cx2 = square(cx);
-        let cy2 = square(cy);
-        let C = cx2 + cy2;
-        let r2 = square(r);
-        let D = C - r2 + 1.;
-        let a = 4. * C;
-        let b: Tensor<'_, f64> = -4. * cx * D;
-        let c = square(D) - 4. * cy2;
-
-        let d: Tensor<'_, f64> = square(b) - 4.*a*c;
-        let dsqrt = sqrt(d);
-        let rhs = dsqrt / 2. / a;
-        let lhs = neg(b) / 2. / a;
-
-        let ixdyn = IxDyn(&[]);
-
-        let x0: Tensor<f64> = lhs - rhs;
-        let y0sq = 1. - square(x0);
-        let y0_0 = neg(sqrt(y0sq));
-        let y0_1 = sqrt(y0sq);
-        let x0cx2 = square(x0 - cx);
-        let check0_0 = abs(x0cx2 + square(y0_0 - cy) - r2).eval(g).unwrap()[&ixdyn];
-        let check0_1 = abs(x0cx2 + square(y0_1 - cy) - r2).eval(g).unwrap()[&ixdyn];
-        let y0 = if check0_0 < check0_1 { y0_0 } else { y0_1 };
-        println!("checks0: {:?}, {:?}", check0_0, check0_1);
-
-        let x1 = lhs + rhs;
-        let y1sq = 1. - square(x1);
-        let y1_0 = neg(sqrt(y1sq));
-        let y1_1 = sqrt(y1sq);
-        let x1cx2 = square(x1 - cx);
-        let check1_0 = abs(x1cx2 + square(y1_0 - cy) - r2).eval(g).unwrap()[&ixdyn];
-        let check1_1 = abs(x1cx2 + square(y1_1 - cy) - r2).eval(g).unwrap()[&ixdyn];
-        let y1 = if check1_0 < check1_1 { y1_0 } else { y1_1 };
-        println!("checks1: {:?}, {:?}", check1_0, check1_1);
-
-        let eval = |&y, name: &str| {
-            let d = &grad::<'_, f64, Tensor<f64>, Tensor<f64>>(&[y], &[cx, cy, r]);
-            let val = y.eval(g).unwrap();
-            let ran =
-                g
-                .evaluator()
-                .push(d[0]).feed(cx, arr0(1.).view())
-                .push(d[1]).feed(cy, arr0(1.).view())
-                .push(d[2]).feed( r, arr0(1.).view())
-                .run();
-            println!("{}: {}", name, val);
-            println!("\tcx {}", &ran[0].as_ref().unwrap()[&ixdyn]);
-            println!("\tcy {}", &ran[1].as_ref().unwrap()[&ixdyn]);
-            println!("\tr  {}", &ran[2].as_ref().unwrap()[&ixdyn]);
-        };
-
-        eval(&x0, "x0");
-        eval(&y0, "y0");
-        eval(&x1, "x1");
-        eval(&y1, "y1");
-    });
+    let (cx, cy, r) = (1., 1., 2.);
+    let (value, grad) = jacobian(|v| f(v[0], v[1], v[2]), SMatrix::from([cx, cy, r]));
+    println!("{value} {grad}");
 }
