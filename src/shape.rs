4903e3b8f4ea1cc31e7354a5d0029f786669b293
Ryan Williams <ryan@runsascoded.com>
2023-09-06 11:52:59 -0400

    serde Shape, rm dead code

--- src/shape.rs
+++ src/shape.rs
@@ -2,16 +2,16 @@ use std::{rc::Rc, cell::RefCell, ops::{Mul, Add}};
 
 use derive_more::{From, Display};
 use serde::{Deserialize, Serialize};
-use tsify::declare;
+use tsify::{declare, Tsify};
 
-use crate::{dual::D, circle, ellipses::xyrr, zero::Zero, transform::{HasProjection, Transform, CanProject, CanTransform}, r2::R2};
+use crate::{dual::D, circle, ellipses::xyrr, zero::Zero, transform::{Transform, CanTransform}, r2::R2};
 
 #[declare]
 pub type Duals = Vec<Vec<f64>>;
 #[declare]
 pub type Input = (Shape<f64>, Duals);
 
-#[derive(Debug, Display, Clone, From, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Display, Clone, From, PartialEq, Serialize, Deserialize, Tsify)]
 pub enum Shape<D> {
     Circle(circle::Circle<D>),
     XYRR(xyrr::XYRR<D>),
@@ -63,49 +63,6 @@ impl Shape<D> {
             Shape::XYRR(e) => Zero::zero(&e.c.x),
         }
     }
-    // pub fn intersect(&self, other: &Shape<D>) -> Vec<R2<D>> {
-    //     match (self, other) {
-    //         (Shape::Circle(c0), Shape::Circle(c1)) => c0.intersect(&c1),
-    //         (Shape::XYRR(e), other) => e.intersect(&other),
-    //         (other, Shape::XYRR(e)) => e.intersect(&other),
-    //     }
-    // }
-
-    // pub fn apply(&self, projection: &Projection<D>) -> Shape<D> {
-    //     projection.0.iter().fold(*self, |c, t| c.transform(t))
-    // }
-    // pub fn projection(&self) -> Projection<D> {
-    //     match self {
-    //         Shape::Circle(c) => c.projection(),
-    //         Shape::XYRR(e) => e.projection(),
-    //     }
-    // }
-    pub fn project(&self, o: &Shape<D>) -> Self {
-        let projection = self.projection();
-        (*o).apply(&projection)
-    }
-    // pub fn intersect(&self, o: &Shape<D>) -> Vec<Intersection<D>> {
-    //     let c0 = self;
-    //     let projection = self.projection();
-    //     let projected = (*o).apply(&projection);
-    //     let unit_intersections = projected.unit_intersections();
-    //     let points = unit_intersections.iter().map(|p| p.apply(&-projection));
-    //     let intersections = points.map(|p| {
-    //         let x = p.x.clone();
-    //         let y = p.y.clone();
-    //         let p = R2 { x: x.clone(), y: y.clone() };
-    //         let t0 = c0.theta(p.clone());
-    //         let t1 = o.theta(p.clone());
-    //         Intersection { x, y, c0idx: c0.idx(), c1idx: o.idx(), t0, t1, }
-    //     });
-    //     intersections.collect()
-    // }
-    // pub fn theta(&self, p: R2<D>) -> D {
-    //     match self {
-    //         Shape::Circle(c) => c.theta(p),
-    //         Shape::XYRR(e) => p.apply(&e.projection()).atan2(),
-    //     }
-    // }
 }
 
 impl<
@@ -130,16 +87,5 @@ where
         }
     }
 }
-
-// impl CanTransform<D> for Shape<D>
-// {
-//     type Output = Shape<D>;
-//     fn transform(&self, transform: &Transform<D>) -> Shape<D> {
-//         match self {
-//             Shape::Circle(c) => c.transform(transform),
-//             Shape::XYRR(e) => Shape::XYRR(e.transform(transform)),
-//         }
-//     }
-// }
 
 pub type S = Rc<RefCell<Shape<D>>>;
