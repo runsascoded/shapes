c271ea210b84148a15f36735db5d462256485855
Ryan Williams <ryan@runsascoded.com>
2023-09-16 08:49:33 -0400

    wip debug nondeterministic diag test

--- src/model.rs
+++ src/model.rs
@@ -345,7 +345,7 @@ mod tests {
             ( Shape::XYRR(XYRR { idx: 1, c: R2 { x: 0., y: 1. }, r: R2 { x: 1., y: 1. } }), vec![ d(3), d(4), d(5), d(6), ] ),
         ];
         // TODO: some nondeterminism sets in on the "error" field, from step 15! Debug.
-        check(inputs, FIZZ_BUZZ.into(), "fizz_buzz_ellipses_diag", 0.7, 14)
+        check(inputs, FIZZ_BUZZ.into(), "fizz_buzz_ellipses_diag", 0.7, 15)
     }
 
     #[test]
